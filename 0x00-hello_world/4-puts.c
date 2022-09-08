2e422e73e727d328e23c16736f62a156bf2adce0
mhegzy <ashilitie3@gmail.com>
2022-09-08 22:33:42 +0000

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,9 +7,7 @@
 */
 int main(void)
 {
-	puts("\"programming is like bu
-	ilding a multilingual puzzle"
-	);
+        puts("\"programming is like building a multilingual puzzle");
 
-	return (0);
+        return (0);
 }
