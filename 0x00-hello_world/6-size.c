13c7ba8304c67fa54a5bff6eed701040525204f8
mhegzy <ashilitie3@gmail.com>
2022-09-12 03:07:16 -0700

    size of datatype

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,22 +1,22 @@
-#include <stdio.h>
-
-/**
- * main - print the sideof var type
- *
- * Return: always 0
- */
-int main(void)
-{
-	int a;
-	char b;
-	long int c;
-	long long int d;
-	float e;
-
-	printf("size of int: %ld byte(s)\n", sizeof(a));
-	printf("size of char: %ld byte(s)\n", sizeof(b));
-	printf("size of long: %ld byte(s)\n", sizeof(c));
-	printf("size of long long: %ld byte(s)\n", sizeof(d));
-	printf("size of float: %ld byte(s)\n", sizeof(e));
-	return (0);
+#include <stdio.h>                                                                                                                      
+                                                                                                                                        
+/**                                                                                                                                     
+ * main - print the sideof var type                                                                                                     
+ *                                                                                                                                      
+ * Return: always 0                                                                                                                     
+ */                                                                                                                                     
+int main(void)                                                                                                                          
+{                                                                                                                                       
+        int a;                                                                                                                          
+        char b;                                                                                                                         
+        long int c;                                                                                                                     
+        long long int d;                                                                                                                
+        float e;                                                                                                                        
+                                                                                                                                        
+        printf("size of int: %ld byte(s)\n", sizeof(a));                                                                                
+        printf("size of char: %ld byte(s)\n", sizeof(b));                                                                               
+        printf("size of long: %ld byte(s)\n", sizeof(c));                                                                               
+        printf("size of long long: %ld byte(s)\n", sizeof(d));                                                                          
+        printf("size of float: %ld byte(s)\n", sizeof(e));                                                                              
+        return (0);                                                                                                                     
 }
